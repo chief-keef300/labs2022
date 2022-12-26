2525e6a825012db5f3ba6224dc7294ae0ea07493
chief-keef300 <95417496+chief-keef300@users.noreply.github.com>
2022-12-26 09:05:06 +0300

    5

---
+++ ShopApi-lab-5/Contracts/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IOrderRepository
+    {
+        IEnumerable<Order> GetOrders(Guid warehouseId, bool trackChanges);
+        Order GetOrder(Guid warehouseId, Guid id, bool trackChanges);
+        void CreateOrderForWarehouse(Guid warehouseId, Order order);
+    }
+}
---
+++ ShopApi-lab-5/LR_WEB_API/Controllers/OrderController.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace LR_WEB_API.Controllers
+{
+    [Route("api/warehouses/{warehouseId}/orders")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+        public OrderController(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult GetOrderForWarehouse(Guid warehouseId)
+        {
+            var warehouse = _repository.Warehouse.GetWarehouse(warehouseId, trackChanges: false);
+            if (warehouse == null)
+            {
+                _logger.LogInfo($"Warehouse with id: {warehouseId} doesn't exist in the database.");
+            return NotFound();
+            }
+           
+            var orderFromDb = _repository.Order.GetOrders(warehouseId,trackChanges: false);
+            var orderDto = _mapper.Map<IEnumerable<OrderDto>>(orderFromDb);
+            return Ok(orderDto);
+
+           
+        }
+
+        [HttpGet("{id}", Name = "GetOrderForWarehouse")]
+        public IActionResult GetOrderForWarehouse(Guid warehouseId, Guid id)
+        {
+            var warehouse = _repository.Warehouse.GetWarehouse(warehouseId, trackChanges: false);
+            if (warehouse == null)
+            {
+                _logger.LogInfo($"Warehouse with id: {warehouseId} doesn't exist in the database.");
+                return NotFound();
+            }
+            var orderDb = _repository.Order.GetOrder(warehouseId, id, trackChanges: false);
+            if (orderDb == null)
+            {
+                _logger.LogInfo($"Order with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+            var order = _mapper.Map<OrderDto>(orderDb);
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrderForWarehouse(Guid warehouseId, [FromBody]
+        OrderForCreationDto order)
+        {
+            if (order == null)
+            {
+                _logger.LogError("OrderForCreationDto object sent from client is null.");
+            return BadRequest("OrderForCreationDto object is null");
+            }
+            var warehouse = _repository.Warehouse.GetWarehouse(warehouseId, trackChanges: false);
+            if (warehouse == null)
+            {
+                _logger.LogInfo($"Warehouse with id: {warehouseId} doesn't exist in the database.");
+            return NotFound();
+            }
+            var orderEntity = _mapper.Map<Order>(order);
+            _repository.Order.CreateOrderForWarehouse(warehouseId, orderEntity);
+            _repository.Save();
+            var orderToReturn = _mapper.Map<OrderDto>(orderEntity);
+            return CreatedAtRoute("GetOrderForWarehouse", new
+            {
+                warehouseId, id = orderToReturn.Id
+            }, orderToReturn);
+        }
+    }
+}
